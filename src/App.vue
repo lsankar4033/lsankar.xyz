9afcc2f53b5822e5a22c70b5d8290ee6f7593c82
lsankar4033 <lsankar4033@gmail.com>
2019-08-19 18:25:58 -0700

    Added Vuetify, started working on layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div id="main">
+    <v-container>
+      <v-row
+        class="mb-6"
+        no-gutters
+      >
+
+        <v-col :cols=4>
+          <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+          </div>
+        </v-col>
+
+        <v-col :cols=auto>
+          <router-view />
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+#main {
+  width: 1000px;
+  margin: 0 auto;
+  color: #3f3844;
+  font-family: monospace;
+  font-size: large;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
