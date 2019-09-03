77850be81c0a7abd7676188a0be40ebba6ae4cc3
lsankar4033 <lsankar4033@gmail.com>
2019-09-02 22:17:48 -0700

    Linting

--- src/views/Past.vue
+++ src/views/Past.vue
@@ -4,24 +4,54 @@
     <br />
 
     <p>
-      I've been fortunate to have had some excellent life experiences so far. In no particular order, here are some
-      of the ones I'm most fond of:
+      I've been fortunate to have had some excellent life experiences so far. In
+      no particular order, here are some of the ones I'm most fond of:
     </p>
     <br />
 
     <ul>
-      <li>2019&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-- running the SF marathon</li>
-      <li>2017&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-- traveling solo without a plan in SE Asia for 3 months</li>
-      <li>2010-2011 -- co-organizing <a href="https://www.youtube.com/watch?v=wkT4QmYkG9g">one of the biggest freestyle hip-hop events</a> in Boston.</li>
-      <li>2006-2008 -- contributing to the search for gravitational waves at <a href="https://www.ligo.org">LIGO</a></li>
-      <li>2007-2011 -- studying Physics and Computer Science at <a href="http://web.mit.edu/">MIT</a></li>
-      <li>2017&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-- building a product from idea to $50k MRR within 5 months at <a href="https://angel.co">AngelList</a></li>
-      <li>2015-2016 -- working on a <a href="https://liftoff.io">machine learning system</a> which made ~600k decisions/second</li>
-      <li>2011-2012 -- co-founding and selling an <a href="https://braingenie.ck12.org">education technology startup</a></li>
-      <li>2017&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-- canoe camping down the Green River in Southern Utah for a week</li>
+      <li>
+        2019&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--
+        running the SF marathon
+      </li>
+      <li>
+        2017&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--
+        traveling solo without a plan in SE Asia for 3 months
+      </li>
+      <li>
+        2010-2011 -- co-organizing
+        <a href="https://www.youtube.com/watch?v=wkT4QmYkG9g"
+          >one of the biggest freestyle hip-hop events</a
+        >
+        in Boston.
+      </li>
+      <li>
+        2006-2008 -- contributing to the search for gravitational waves at
+        <a href="https://www.ligo.org">LIGO</a>
+      </li>
+      <li>
+        2007-2011 -- studying Physics and Computer Science at
+        <a href="http://web.mit.edu/">MIT</a>
+      </li>
+      <li>
+        2017&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--
+        building a product from idea to $50k MRR within 5 months at
+        <a href="https://angel.co">AngelList</a>
+      </li>
+      <li>
+        2015-2016 -- working on a
+        <a href="https://liftoff.io">machine learning system</a> which made
+        ~600k decisions/second
+      </li>
+      <li>
+        2011-2012 -- co-founding and selling an
+        <a href="https://braingenie.ck12.org">education technology startup</a>
+      </li>
+      <li>
+        2017&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--
+        canoe camping down the Green River in Southern Utah for a week
+      </li>
     </ul>
-
-
   </div>
 </template>
 
