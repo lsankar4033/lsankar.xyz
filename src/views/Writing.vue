b4e76ec0feb242e4582e442824521700426f9d97
lsankar4033 <lsankar4033@gmail.com>
2020-06-27 15:34:40 -0700

    Invert post order

--- src/views/Writing.vue
+++ src/views/Writing.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="writing">
     <br />
-
-    <router-link to="/here-we-go-again">Here we go again</router-link> -- June
-    14, 2020
+    <router-link to="/optimizers-idealists-and-adventurers"
+      >Optimizers, idealists, and adventurers</router-link
+    >
+    -- June 26, 2020
 
     <br />
     <br />
@@ -16,10 +17,8 @@
     <br />
     <br />
 
-    <router-link to="/optimizers-idealists-and-adventurers"
-      >Optimizers, idealists, and adventurers</router-link
-    >
-    -- June 26, 2020
+    <router-link to="/here-we-go-again">Here we go again</router-link> -- June
+    14, 2020
   </div>
 </template>
 
