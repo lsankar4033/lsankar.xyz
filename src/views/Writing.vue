504b007de7b8b571da7acd795a53d57bf439905f
lsankar4033 <lsankar4033@gmail.com>
2020-10-10 13:38:18 -0700

    Remove comments I'll probably never use

--- src/views/Writing.vue
+++ src/views/Writing.vue
@@ -1,18 +1,6 @@
 <template>
   <div id="writing">
     <br />
-
-    <!--
-    <p style="text-align: center;">
-      The best way to follow my writing is to follow
-      <a href="https://github.com/lsankar4033/lakshmansankar.com/"
-        >this github repo</a
-      >
-    </p>
-    <p style="text-align: center;">
-      Look for commits starting with "[New Post]"
-    </p>
-    -->
     <div v-for="post in posts" v-bind:key="post.path">
       <br />
       <router-link v-bind:to="post.path">{{ post.title }}</router-link>
