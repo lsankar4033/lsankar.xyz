a3edd9af06b7d080fc3204671477f3ae0bb1099e
lsankar4033 <lsankar4033@gmail.com>
2020-10-10 11:04:39 -0700

    Add Post component

---
+++ src/views/Post.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="post" v-html="body"></div>
+</template>
+
+<script>
+export default {
+  props: ["body"],
+  name: "Post"
+};
+</script>
+
+<style>
+#title {
+  text-align: center;
+  color: #313131;
+}
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 600px;
+}
+</style>
