43ddf39e3ec41dd9cb542b7ec446b7eb43eff542
lsankar4033 <lsankar4033@gmail.com>
2020-10-10 12:38:56 -0700

    WIP porting to new format

---
+++ src/posts.js
@@ -0,0 +1,35 @@
+import b1 from "./posts/1.js";
+import b2 from "./posts/2.js";
+import b3 from "./posts/3.js";
+import b4 from "./posts/4.js";
+import b5 from "./posts/5.js";
+//import b6 from "./posts/6.js";
+//import b7 from "./posts/7.js";
+
+export default [
+  {
+    title: "Here we go again",
+    path: "/here-we-go-again",
+    body: b1
+  },
+  {
+    title: "Changing one thing at a time",
+    path: "/changing-one-thing-at-a-time",
+    body: b2
+  },
+  {
+    title: "Optimizers, idealists, and adventurers",
+    path: "/optimizers-idealists-and-adventurers",
+    body: b3
+  },
+  {
+    title: "Exploring code in space and time",
+    path: "/exploring-code-in-space-and-time",
+    body: b4
+  },
+  {
+    title: "Reconnecting with Nietzsche",
+    path: "/reconnecting-with-nietzsche",
+    body: b5
+  }
+];
--- src/router.js
+++ src/router.js
@@ -3,79 +3,39 @@ import Router from "vue-router";
 import About from "./views/About.vue";
 import Writing from "./views/Writing.vue";
 import WritingCrypto from "./views/WritingCrypto.vue";
-
-import ChangingOneThingAtATime from "./views/Writing/ChangingOneThingAtATime.vue";
-import OptimizersIdealistsAndAdventurers from "./views/Writing/OptimizersIdealistsAndAdventurers.vue";
-import ExploringCodeInSpaceAndTime from "./views/Writing/ExploringCodeInSpaceAndTime.vue";
-import ReconnectingWithNietzsche from "./views/Writing/ReconnectingWithNietzsche.vue";
-import ShardsAsDataAvailabilityLayers from "./views/Writing/ShardsAsDataAvailabilityLayers.vue";
-import FastPipesSmartVMs from "./views/Writing/FastPipesSmartVMs.vue";
-
 import Post from "./views/Post.vue";
 
-import hwga from "./posts/1.js";
+import posts from "./posts.js";
+
+const topLevelRoutes = [
+  {
+    path: "/",
+    name: "about",
+    component: About
+  },
+  {
+    path: "/writing",
+    name: "writing",
+    component: Writing
+  },
+  {
+    path: "/writing-crypto",
+    name: "writing-crypto",
+    component: WritingCrypto
+  }
+];
+
+const postRoutes = [];
+for (const post of posts) {
+  postRoutes.push({
+    path: post.path,
+    component: Post,
+    props: { body: post.body }
+  });
+}
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: "/",
-      name: "about",
-      component: About
-    },
-    {
-      path: "/writing",
-      name: "writing",
-      component: Writing
-    },
-    {
-      path: "/writing-crypto",
-      name: "writing-crypto",
-      component: WritingCrypto
-    },
-    {
-      path: "/here-we-go-again",
-      component: Post,
-      props: { body: hwga }
-    },
-    {
-      path: "/changing-one-thing-at-a-time",
-      name: "changing-one-thing-at-a-time",
-      component: ChangingOneThingAtATime
-    },
-    {
-      path: "/optimizers-idealists-and-adventurers",
-      name: "optimizers-idealists-and-adventurers",
-      component: OptimizersIdealistsAndAdventurers
-    },
-    {
-      path: "/exploring-code-in-space-and-time",
-      name: "exploring-code-in-space-and-time",
-      component: ExploringCodeInSpaceAndTime
-    },
-    {
-      path: "/reconnecting-with-nietzsche",
-      name: "reconnecting-with-nietzsche",
-      component: ReconnectingWithNietzsche
-    },
-    {
-      path: "/shards-as-data-availability-layers",
-      name: "shards-as-data-availability-layers",
-      component: ShardsAsDataAvailabilityLayers
-    },
-    {
-      path: "/fast-pipes-smart-vms",
-      name: "fast-pipes-smart-vms",
-      component: FastPipesSmartVMs
-    },
-
-    // TODO: remove
-    {
-      path: "/test-post",
-      name: "test-post",
-      component: Post,
-      props: { body: "<div><p>line 1</p><p>line 2</p></div>" }
-    }
-  ]
+  routes: topLevelRoutes.concat(postRoutes)
 });
