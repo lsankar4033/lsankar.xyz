5564bd10d973884abca79dee7fcc37b86f5e890b
lsankar4033 <lsankar4033@gmail.com>
2021-01-13 19:54:55 -0800

    Add e-mail subscription for real

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,24 @@
         </v-col>
       </v-row>
 
+      <br />
+      <hr />
+      <br />
+
+      <div id="subscribe">
+      <p>
+        Interested in being notified when I write a new post? Subscribe below:
+      </p>
+      <br />
+      <br />
+
       <vue-mailchimp-email-signup-form
         :element-id="'email-signup'"
         :url="
           'https://gmail.us7.list-manage.com/subscribe/post?u=1417363863053479cba16f5df&id=e02324eaa4'
         "
       />
+      </div>
     </v-container>
   </div>
 </template>
@@ -34,6 +46,30 @@ export default {
 </script>
 
 <style>
+#subscribe {
+  text-align: center;
+  font-size: 14px;
+}
+.subscribe-container {
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 300px;
+}
+
+#email-signupmce-EMAIL {
+  font-size: 14px;
+}
+
+#email-signupmc-embedded-subscribe {
+  font-size: 14px;
+  background-color: #999;
+}
+
+
+#email-signupmc-embedded-subscribe:hover {
+  background-color: #aaa;
+}
+
 #container {
   max-width: 600px;
 }
