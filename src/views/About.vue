430eef2938916f04a15ccd2c3a8c63a8f57e7b6d
lsankar4033 <lsankar4033@gmail.com>
2021-07-28 16:23:25 -0700

    lint

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,8 @@
     <br />
 
     <p>
-      Hi, I'm Lakshman. I do R&D at the <a href="https://ethereum.org/">Ethereum Foundation</a>. I'm interested in
+      Hi, I'm Lakshman. I do R&D at the
+      <a href="https://ethereum.org/">Ethereum Foundation</a>. I'm interested in
       making sure that the protocol serves the application layer. I also like
       hacking (<a href="https://github.com/lsankar4033/">Github</a>) and (very)
       occasionally angel investing/consulting with teams that inspire me.
@@ -12,10 +13,10 @@
     <br />
 
     <p>
-      When it comes to building a free society, I believe there's a
-      bad way and a good way to build systems. I'm committed to exploring the
-      good way. I think we should all have a choice in which systems and
-      institutions to be a part of.
+      When it comes to building a free society, I believe there's a bad way and
+      a good way to build systems. I'm committed to exploring the good way. I
+      think we should all have a choice in which systems and institutions to be
+      a part of.
     </p>
 
     <br />
