d02725458822d2e8a3f5b014ec8702af0814b4c7
lsankar4033 <lsankar4033@gmail.com>
2021-12-20 12:35:55 -0800

    lint

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,25 +4,34 @@
 
     <p>
       Hi, I'm Lakshman. I'm an <a href="https://ethereum.org/">Etherean</a>.
-      I've spent the last couple years working in R&D and strategy at the Ethereum Foundation.
+      I've spent the last couple years working in R&D and strategy at the
+      Ethereum Foundation.
     </p>
 
     <p>
-      Most recently, I drove the thinking around supporting the budding proof-of-stake ecosystem after the launch of the <a href="https://ethereum.org/en/eth2/beacon-chain/">beacon chain</a>.
-      One piece of legible work to come out of this was <a href="https://blog.ethereum.org/2021/02/09/esp-staking-community-grantee-announcement/">a series of grants</a>. I was also deeply involved in staking pool R&D and thinking through how MEV affects L1 protocol development.
+      Most recently, I drove the thinking around supporting the budding
+      proof-of-stake ecosystem after the launch of the
+      <a href="https://ethereum.org/en/eth2/beacon-chain/">beacon chain</a>. One
+      piece of legible work to come out of this was
+      <a
+        href="https://blog.ethereum.org/2021/02/09/esp-staking-community-grantee-announcement/"
+        >a series of grants</a
+      >. I was also deeply involved in staking pool R&D and thinking through how
+      MEV affects L1 protocol development.
     </p>
 
     <p>
-      I'm currently deep in the 'crypto' startup idea maze. The industry is evolving and the time couldn't be better to
-      double down on a thesis and build something of decade-scale importance.
+      I'm currently deep in the 'crypto' startup idea maze. The industry is
+      evolving and the time couldn't be better to double down on a thesis and
+      build something of decade-scale importance.
     </p>
 
     <br />
 
     <p>
       If you want to work with me, get in touch. I also occassionally invest in
-      or advise teams that inspire me and am happy to connect in that capacity as
-      well.
+      or advise teams that inspire me and am happy to connect in that capacity
+      as well.
     </p>
 
     <br />
