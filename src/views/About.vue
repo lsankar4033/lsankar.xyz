54924b70093ee03d4d84e8b5645a5c7a7af8d07d
lsankar4033 <lsankar4033@gmail.com>
2022-06-09 06:51:57 -0700

    Small changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,29 +5,15 @@
     <p>
       Hi, I'm Lakshman. I'm an <a href="https://ethereum.org/">Etherean</a>.
       I've spent the last couple years working in R&D and strategy at the
-      Ethereum Foundation.
+      Ethereum Foundation. Prior to that, I was a product/engineering leader at a number of silicon valley startups, some of my own included. 
     </p>
 
     <br />
-
-    <p>
-      Most recently, I drove much of the thinking around supporting the budding
-      proof-of-stake ecosystem after the launch of the
-      <a href="https://ethereum.org/en/eth2/beacon-chain/">beacon chain</a>. One
-      piece of legible work to come out of this was
-      <a
-        href="https://blog.ethereum.org/2021/02/09/esp-staking-community-grantee-announcement/"
-        >a series of grants</a
-      >. I was also deeply involved in staking pool R&D and thinking through how
-      MEV affects L1 protocol development, and ZKP app R&D.
-    </p>
-
-    <br />
-
+    
     <p>
       I'm currently deep in the 'crypto' startup idea maze. The industry is
       evolving and the time couldn't be better to double down on a vision and
-      build something of century-scale importance.
+      build something *new* of century-scale importance. 
     </p>
 
     <br />
