683fe366d357ad496c468e3013fc2599475b9d3b
lsankar4033 <lsankar4033@gmail.com>
2022-10-08 11:05:47 -0700

    lint

--- src/views/About.vue
+++ src/views/About.vue
@@ -12,10 +12,10 @@
     <br />
 
     <p>
-      I'm currently working on <a href="https://personaelabs.org/">Personae Labs</a>.
-      We're building technology and products to better understand how the
-      productionization of new cryptography like zk-SNARKs changes the way we
-      relate online.
+      I'm currently working on
+      <a href="https://personaelabs.org/">Personae Labs</a>. We're building
+      technology and products to better understand how the productionization of
+      new cryptography like zk-SNARKs changes the way we relate online.
     </p>
 
     <br />
