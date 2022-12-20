137b8bb3b985e586ce9e9312889f9ceec75c4420
lsankar4033 <lsankar4033@gmail.com>
2022-12-19 21:53:35 -0800

    Casing fix

--- src/posts.js
+++ src/posts.js
@@ -25,139 +25,139 @@ export default [
     path: "/here-we-go-again",
     body: b1,
     category: "life",
-    date: new Date("06/14/20")
+    date: new Date("06/14/20"),
   },
   {
     title: "Changing one thing at a time",
     path: "/changing-one-thing-at-a-time",
     body: b2,
     category: "life",
-    date: new Date("06/18/20")
+    date: new Date("06/18/20"),
   },
   {
     title: "Optimizers, idealists, and adventurers",
     path: "/optimizers-idealists-and-adventurers",
     body: b3,
     category: "life",
-    date: new Date("06/26/20")
+    date: new Date("06/26/20"),
   },
   {
     title: "Exploring code in space and time",
     path: "/exploring-code-in-space-and-time",
     body: b4,
     category: "life",
-    date: new Date("07/08/20")
+    date: new Date("07/08/20"),
   },
   {
     title: "Reconnecting with Nietzsche",
     path: "/reconnecting-with-nietzsche",
     body: b5,
     category: "life",
-    date: new Date("07/24/20")
+    date: new Date("07/24/20"),
   },
   {
     title: "Simplifying Eth 2.0: Shards as data availability layers",
     path: "/shards-as-data-availability-layers",
     body: b6,
     category: "crypto",
-    date: new Date("09/04/20")
+    date: new Date("09/04/20"),
   },
   {
     title: "Unrolling Rollups; Fast pipes and smart VMs",
     path: "/fast-pipes-smart-vms",
     body: b7,
     category: "crypto",
-    date: new Date("09/27/20")
+    date: new Date("09/27/20"),
   },
   {
     title: "Internet conversations",
     path: "/internet-conversations",
     body: b8,
     category: "life",
-    date: new Date("10/10/20")
+    date: new Date("10/10/20"),
   },
   {
     title: "On decentralized Eth2 staking pools",
     path: "/staking-pools",
     body: b9,
     category: "crypto",
-    date: new Date("12/07/20")
+    date: new Date("12/07/20"),
   },
   {
     title: "Legibility and Curiosity",
     path: "/legis-curis",
     body: b10,
     category: "life",
-    date: new Date("12/14/20")
+    date: new Date("12/14/20"),
   },
   {
     title: "Satoshi's Dreams",
     path: "/satoshis-dreams",
     body: b11,
     category: "crypto",
-    date: new Date("01/11/21")
+    date: new Date("01/11/21"),
   },
   {
     title: "Mimetic Reading",
     path: "/mimetic-reading",
     body: b12,
     category: "life",
-    date: new Date("01/26/21")
+    date: new Date("01/26/21"),
   },
   {
     title: "Developing crypto ecosystems",
     path: "/crypto-ecosystems",
     body: b13,
     category: "crypto",
-    date: new Date("01/31/21")
+    date: new Date("01/31/21"),
   },
   {
     title: "Keeping up with crypto",
     path: "/keeping-up-crypto",
     body: b14,
     category: "crypto",
-    date: new Date("06/16/21")
+    date: new Date("06/16/21"),
   },
   {
     title: "Inter-chain and Intra-chain MEV",
     path: "/inter-intra-mev",
     body: b15,
     category: "crypto",
-    date: new Date("06/24/21")
+    date: new Date("06/24/21"),
   },
   {
     title: "On categorization",
     path: "/on-categorization",
     body: b16,
     category: "life",
-    date: new Date("07/07/21")
+    date: new Date("07/07/21"),
   },
   {
     title: "The tradeoffs of cross-chain communication",
     path: "/tradeoffs-ccc",
     body: b17,
     category: "crypto",
-    date: new Date("09/10/21")
+    date: new Date("09/10/21"),
   },
   {
     title: "Tech leadership",
     path: "/tech-leadership",
     body: b18,
     category: "life",
-    date: new Date("02/11/22")
+    date: new Date("02/11/22"),
   },
   {
-    title: "widely used logic vs. valuable dataset",
+    title: "Widely used logic vs. valuable dataset",
     path: "/logic-dataset",
     body: b19,
     category: "crypto",
-    date: new Date("05/22/22")
+    date: new Date("05/22/22"),
   },
   {
-    title: "the excellent IC's dilemma",
+    title: "The excellent IC's dilemma",
     path: "/excellent-ic",
     body: b20,
     category: "life",
-    date: new Date("12/19/22")
-  }
+    date: new Date("12/19/22"),
+  },
 ];
